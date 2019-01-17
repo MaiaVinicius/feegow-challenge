4a2018dd5c739fb4a17f651bbb256016df7ed07b
Thales Nathan <thcmatias@gmail.com>
2019-01-17 20:31:46 -0200

    Criando o modelo "Formulário"
    Chamando-o de "Schedule" no projeto, pois "Form" poderia causar uma certa confusão.

---
+++ database/migrations/2019_01_17_220757_create_schedules_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('user_id');
+
+            $table->integer('specialty_id');
+            $table->integer('professional_id');
+            $table->integer('source_id');
+
+            $table->string('name');
+            $table->char('cpf', 11)->unique();
+            $table->date('birthdate');
+            $table->dateTime('date_time')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
